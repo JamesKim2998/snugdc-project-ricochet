8cc6bea62b25195723ab5e223b6bb1e6d718daa4
geoidn2 <eodnjs2998@gmail.com>
2014-07-01 22:49:07 +0900

    Skeleton 프로젝트.

---
+++ Skeleton/Assets/Scripts/Character/WeaponAimPivot.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WeaponAimPivot : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void OnDrawGizmos () {
+#if UNITY_EDITOR
+		for (var _angle = -90; _angle <= 90; _angle += 15)
+		{
+			if (_angle % 45 == 0) {
+				Gizmos.color = Color.blue;
+			} else {
+				Gizmos.color = Color.white;
+			}
+
+			var _angleRad = Mathf.Deg2Rad * _angle;
+
+			Gizmos.DrawLine(transform.position, 
+			                transform.position + 15 * new Vector3(Mathf.Cos(_angleRad), Mathf.Sin(_angleRad)));
+		}
+		
+		for (var _radius = 1; _radius < 15; ++_radius) 
+		{
+			if (_radius % 5 == 0) {
+				Handles.color = Color.blue;
+			} else {
+				Handles.color = Color.white;
+			}
+
+			Handles.DrawWireArc(
+				transform.position,
+				transform.forward,
+				-transform.up,
+				180,
+				_radius);
+		}
+#endif
+	}
+}
---
+++ Skeleton/Assets/Scripts/Skeleton/JointPlacer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JointPlacer : MonoBehaviour {
+	
+	public bool clockwise;
+
+	public Bone root;
+	public Bone joint;
+	public Helper attachment;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		var _root_position = Vector3.zero;
+		var _attachment_position = root.transform.worldToLocalMatrix.MultiplyPoint3x4(attachment.transform.position);
+
+		var _v = (_root_position - _attachment_position);
+		var _d = _v.magnitude;
+		var _l1 = root.length;
+		var _l2 = joint.length;
+
+		if (_d > _l1 + _l2) _d = _l1 + _l2;
+
+		var _a1 = Mathf.Atan2(_v.x, -_v.y);
+		var _a2 = Mathf.Acos((_d * _d + _l1 * _l1 - _l2 * _l2) / (2 * _d * _l1));
+
+		var _a = _a1;
+		_a += clockwise ? _a2 : -_a2;
+
+		transform.position = root.transform.localToWorldMatrix.MultiplyPoint3x4(_l1 * new Vector3(-Mathf.Sin(_a), Mathf.Cos(_a), 0));
+	}
+}
