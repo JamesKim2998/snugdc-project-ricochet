cb89711a079ebe132bed47baf3aaff642daa9566
Juhyeong Park <majecty@gmail.com>
2014-08-26 23:49:13 +0900

    Make text box that shows name above head.

---
+++ SuperCrateBox/Assets/Scripts/GameObject/Character/CharacterName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterName : MonoBehaviour {
+
+	public TextMesh text;
+
+	void SetName(string _name)
+	{
+		text.text = _name;
+	}
+}
