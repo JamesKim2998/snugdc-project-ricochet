4b1ace7cb6bc7bb34acd04e8e4c491551eb7d880
geoidn2 <eodnjs2998@gmail.com>
2014-09-20 22:45:49 +0900

    muzzle_fire 사운드 추가.

---
+++ Assets/Scripts/Sound/PlaySFX.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySFX : MonoBehaviour
+{
+    public bool playOnStart = true;
+    public bool deleteAfterPlay = true;
+
+	void Start ()
+	{
+	    if (playOnStart)
+            Play();
+	}
+
+    public void Play()
+    {
+        if (!Global.SFX().mute)
+            audio.Play();
+
+        if (deleteAfterPlay)
+            Destroy(this);
+    }
+}
