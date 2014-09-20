71f1cbf1b322a37b76e6d8c69d3cd8cff152934e
geoidn2 <eodnjs2998@gmail.com>
2014-09-21 01:57:37 +0900

    save name into pref.

--- Assets/Scripts/Global/PlayerManager.cs
+++ Assets/Scripts/Global/PlayerManager.cs
@@ -8,9 +8,11 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerInfo
 {
-	public string guid = "";
+    public string guid { get; private set; }
 
     public bool connected = false;
+
+    public bool isMine { get { return Network.player.guid == guid; } }
 
     private string m_Name = "undefined";
 	public string name { 
@@ -18,6 +20,7 @@ public class PlayerInfo
 		set { 
 			if (m_Name == value) return; 
 			m_Name = value; 
+            if (isMine) PlayerPrefs.SetString("player_name", m_Name);
 			if (postChanged != null) postChanged(this); 
 		}
 	}
@@ -34,6 +37,13 @@ public class PlayerInfo
 
 	[System.NonSerialized]
 	public Action<PlayerInfo> postChanged;
+
+    public PlayerInfo(string _guid)
+    {
+        guid = _guid;
+        if (isMine)
+            m_Name = PlayerPrefs.GetString("player_name", "team_ricochet");
+    }
 }
 
 public class PlayerManager : MonoBehaviour
@@ -66,9 +76,9 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    void Awake() 
-	{
-		mine = new PlayerInfo {guid = Network.player.guid, name = Network.player.guid};
+    void Awake()
+    {
+        mine = new PlayerInfo(Network.player.guid);
 	    players = new Dictionary<string, PlayerInfo> { {Network.player.guid, mine} };
 	}
 
@@ -104,7 +114,7 @@ public class PlayerManager : MonoBehaviour
 	    }
 	    else
 	    {
-            _playerInfo = new PlayerInfo {guid = _player, name = _player.ToString() };
+	        _playerInfo = new PlayerInfo(Network.player.guid);
 	        Add(_playerInfo);
             return _playerInfo;
         }
