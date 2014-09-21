fe9484a2fd15f4f537ebb1675dce670a3480847a
geoidn2 <eodnjs2998@gmail.com>
2014-09-21 09:05:24 +0900

    anchor9 추가.

---
+++ Assets/UI/Anchor/UIAnchor9.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class UIAnchor9 : MonoBehaviour {
+    public List<UIAnchor> anchors;
+
+    public GameObject AddPrf(GameObject _prf, Anchor9 _anchor)
+    {
+        var _go = (GameObject) Instantiate(_prf);
+        return null;
+    }
+}
